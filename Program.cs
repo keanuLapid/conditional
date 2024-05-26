6c780e6d043ff96135aba3f0f385fa36a2d35a7e
keanuLapid <keanu@csmbataan.ph.education>
2024-05-27 00:20:17 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int age;
+
+        Console.Write("Enter your age: ");
+        age = Convert.ToInt32(Console.ReadLine());
+
+        if (age >= 18)
+        {
+            Console.WriteLine("Congratulation! You are eligible for casting your vote.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you are not eligible to cast your vote.");
+        }
+
+        Console.ReadLine(); 
+    }
+}
